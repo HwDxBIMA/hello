a969224f9ec9c72a03872c618c79677ddd55a52d
HwDxBIMA <bnagantara@gmail.com>
2019-03-02 20:40:17 +0700

    update tapi bohong

--- for.cpp
+++ for.cpp
@@ -1,10 +1,14 @@
 #include <iostream>
+#include <windows.h>
 using namespace std;
 int main(){
-  int angka=10;
-  for(int i=0;i<angka;i++){
-    cout << "saya cinta pemrograman" <<endl;
+  for(int i=0;i<1;i++){
+    cout << "		saya cinta pemrograman		" <<endl;
+    Sleep(700);
   }
-  cout << "everithing is fake";
+	
+	Sleep(1000);
+  	cout << "		     Tapi Bohong	     	" <<endl;
+  	cout << "	   	   hiya! ";Sleep(600);cout << "hiya! ";Sleep(600);cout << "hiya! ";Sleep(600);
   return 0;
 }
