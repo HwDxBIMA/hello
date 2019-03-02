c7a9310cb9b534d1162cc1be7467275dca718897
HwDxBIMA <47941792+HwDxBIMA@users.noreply.github.com>
2019-03-02 20:18:16 +0700

    Coba Update Local
    Coba Update

---
+++ for.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main(){
+  int angka=10;
+  for(int i=0;i<angka;i++){
+    cout << "saya cinta pemrograman" <<endl;
+  }
+  cout << "everithing is fake";
+  return 0;
+}
